6bb374adeb761c73b143422ddffe91503b0b7670
ZingP <605156398@qq.com>
2018-03-09 16:56:21 +0800

    格式化输出新特性

---
+++ 黑魔法/字符串新特性.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: "liuyouyuan"
+# Date: 2018/3/9
+
+name = "liuyouyuan"
+age = "18"
+
+# 用 format 的方式格式化输出字符串
+print("{name} age is {age}".format(name=name, age=age))
+
+# 新姿势 格式化输出字符串
+print(f"{name} age is {age}")
