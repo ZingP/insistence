781e0bd0294664cdd96b90c8fac6c5da3884ee79
ZingP <605156398@qq.com>
2018-03-14 13:00:16 +0800

    使用yield语句，实现简单任务调度器

--- 网络编程/协程/协程1_yield语句.py
+++ 网络编程/协程/协程1_yield语句.py
@@ -9,23 +9,51 @@
 (2) 使用yield语法的函数是一个生成器；
 (3) python3中，通过 .__next__() 或者 next() 方法获取生成器的下一个值。
 """
-def countdown(n):
+from collections import deque
+
+def sayHello(n):
     while n > 0:
-        print('T-minus', n)
+        print("hello~", n)
         yield n
         n -= 1
-    print('Blastoff!')
+    print('say hello')
 
-def countup(n):
+def sayHi(n):
     x = 0
     while x < n:
-        print('Counting up', x)
+        print('hi~', x)
         yield
         x += 1
-x = countdown(10)
-print(x.__next__())
-print(next(x))
-x.__next__()
-# x.__next__()
+    print("say hi")
 
+# 使用yield语句，实现简单任务调度器
+class TaskScheduler(object):
+    def __init__(self):
+        self._task_queue = deque()
+
+    def new_task(self, task):
+        '''
+        向调度队列添加新的任务
+        '''
+        self._task_queue.append(task)
+
+    def run(self):
+        '''
+        不断运行，直到队列中没有任务
+        '''
+        while self._task_queue:
+            task = self._task_queue.popleft()
+            try:
+                next(task)
+                self._task_queue.append(task)
+            except StopIteration:
+                # 生成器结束
+                pass
+
+# Example use
+sched = TaskScheduler()
+sched.new_task(sayHello(10))
+sched.new_task(sayHello(5))
+sched.new_task(sayHi(15))
+sched.run()
 
