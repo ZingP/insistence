89e77ceb268e0893af44c0a4a8d002da1f1cac1d
ZingP <605156398@qq.com>
2018-03-14 16:10:36 +0800

    3.6新特性

---
+++ 新特性/函数注释.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: "liuyouyuan"
+# Date: 2018/3/14
+
+
+def sayHi(name: 'your name') -> 'say hi':
+    print(f"Hi {name}")
+    return
+
+def func(x, y) -> ['hhh']:
+    return x + y
+
+sayHi("liuyouyuan")
+a = func(1, 2)
+print(a, type(a))
+print(func.__annotations__)
+print(sayHi.__annotations__)
+
+# 在数值文字中使用下划线的能力
+print('{:_}'.format(1000000))
+
