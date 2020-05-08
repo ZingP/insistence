6ed37995965c173e32c8518eedb3e28a6f3cd443
zingp <605156398@qq.com>
2020-05-08 11:59:20 +0800

    读写excel

--- 操作excel/写.py
+++ 操作excel/写.py
@@ -1,71 +1,33 @@
-import sys
-import json
-import openpyxl
-# 通常写excel使用下面这个模块
+import xlrd
 import xlsxwriter
 
-def load_data(data_file):
-    """加载数据"""
-    data_list = []
-    with open(data_file, "r") as f:
-        for line in f:
-            data_dic = json.loads(line)
-            data_list.append(data_dic)
-    return data_list
 
+def read_excel_row(excelname):
+    li = []
+    hash_set = set()
+    workbook = xlrd.open_workbook(excelname)
+    sheet = workbook.sheet_by_name('Sheet1')
+    for i in range(1, sheet.nrows):
+        row = sheet.row_values(i)
+        topic = row[2]
+        if topic in hash_set:
+            continue
+        hash_set.add(topic)
+        li.append(row)
+    return li
 
-def list2excel(data_list, file_path):
-    fp = openpyxl.Workbook() #创建工作簿
-    sheet1 = fp.create_sheet()
-    column_names = ["中心词", "节目id", "节目名称","节目摘要"]
-    # 将列名写入第一行
-    for i in range(len(column_names)):
-        sheet1.cell(row=1, column=i+1).value = column_names[i]
-
-    # 将数据写入
-    for i, item in enumerate(data_list):
-        
-        center_word = item["center_word"]
-        audio_id  = item["audio_id"]
-        audio_name = item["audio_name"]
-        audio_info = item["audio_info"]
-
-        sheet1_row = i+2
-        sheet1.cell(row=sheet1_row, column=1).value = center_word
-        sheet1.cell(row=sheet1_row, column=2).value = audio_id
-        sheet1.cell(row=sheet1_row, column=3).value = audio_name
-        sheet1.cell(row=sheet1_row, column=4).value = audio_info
-    fp.save(file_path)
-
-
-def write2excel(ata_list, file_path):
-    """写Excel"""
+def write2excel(data_list, file_path, column_names):
     workbook = xlsxwriter.Workbook(file_path)
     sheet1 = workbook.add_worksheet()
-    column_names = ["中心词", "节目id", "节目名称","节目摘要"]
+    #column_names = ["中心词", "节目id", "节目名称","节目摘要"]
     for i in range(len(column_names)):
         sheet1.write(0, i, column_names[i])
-        # 将数据写入
-    for i, item in enumerate(data_list):
-        
-        center_word = item["center_word"]
-        audio_id  = item["audio_id"]
-        audio_name = item["audio_name"]
-        audio_info = item["audio_info"]
-
+    # 将数据写入
+    for i, row_li in enumerate(data_list):
         sheet1_row = i+1
-        sheet1.write(sheet1_row, 0, center_word)
-        sheet1.write(sheet1_row, 1, audio_id)
-        sheet1.write(sheet1_row, 2, audio_name)
-        sheet1.write(sheet1_row, 3, audio_info)
+        for j in range(len(row_li)):
+            print(sheet1_row, j, row_li[j])
+            sheet1.write(sheet1_row, j, row_li[j])
     workbook.close()
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Useage: python3 json2excel.py filename")
-    opt = sys.argv[1:]
-    data_file = opt[0]
-    excel_file = "{}.xlsx".format(data_file)
-    data_list = load_data(data_file)
-    write2excel(data_list, excel_file)
-    print("Translating {} to {} done!".format(data_file, excel_file))
+    
--- 操作excel/读.py
+++ 操作excel/读.py
@@ -1,73 +1,16 @@
-import xlrd  # 读一般使用这个
-import openpyxl
-import json
+import xlrd
+import xlsxwriter
 
-f = open("admins.json", "r", encoding="utf-8")
-admin_dic = json.load(f)
-f.close()
-sn_admin_dic = admin_dic["data"]
-
-workbook = xlrd.open_workbook(r'./_1_hosts_list.xlsx')
-# 获取所有sheet
-sheet_name = workbook.sheet_names()[1]
-# print(sheet_name)
-
-# 根据sheet索引或者名称获取sheet内容
-sheet = workbook.sheet_by_name('Sheet2')
-# print(sheet.cell_value(0,1))
-# print (sheet.name,sheet.nrows,sheet.ncols)
-index_name = sheet.row_values(0) # 获取第1行内容
-# print(index_name)
-new_index = ['机群ID', '机群名称', '机器类别（vps虚机；srvs实机）', 'IP', 'SN']
-#index_name = ['ID', 'PROJECT_CODE', 'PROJECT_NAME', 'TIME', 'TIME_UNIT', 'UPDATE_TIME', '机群ID', '机群名称', '机器类别（vps虚机；srvs实机）', 'DOMAIN', 'IP', 'SN', 'CPU_IDLE_RATE', 'MEM_IDLE_RATE', 'DISK_IDLE_RATE', 'GPU_IDLE_RATE', 'SERVER_IDLE_RATE']
-# for i in new_index:
-#     print(i, index_name.index(i))
-
-fp = openpyxl.Workbook() #创建工作簿
-sheet1 = fp.create_sheet()
-
-new_index_name = ['SN', '机群ID', '机群名称', '机器类别（vps虚机；srvs实机）', 'IP', '机器管理员']
-for i in range(len(new_index_name)):
-    sheet1.cell(row=1, column=i+1).value = new_index_name[i]
-
-mail_set = set()
-for i in range(1, sheet.nrows):
-    row = sheet.row_values(i)
-    # sn号
-    sn = row[11]
-    # 集群ID
-    cluster_id = row[6]
-    # 机群名称
-    cluster_name = row[7]
-
-    # 机器类别
-    host_class = row[8]
-    if host_class.strip() == 'vps':
-        h_class = "虚机"
-    elif host_class.strip() == 'srv':
-        h_class = "实机"
-    else:
-        h_class = host_class
-    # ip
-    ip = row[10]
-    # 机器管理员
-    ip_admins = sn_admin_dic[sn]
-    host_admins = ",".join(ip_admins)
-    mail_set.add(host_admins)
-
-    sheet1_row = i+1
-    sheet1.cell(row=sheet1_row, column=1).value = sn
-    sheet1.cell(row=sheet1_row, column=2).value = cluster_id
-    sheet1.cell(row=sheet1_row, column=3).value = cluster_name
-    sheet1.cell(row=sheet1_row, column=4).value = h_class
-    sheet1.cell(row=sheet1_row, column=5).value = ip
-    sheet1.cell(row=sheet1_row, column=6).value = host_admins
-fp.save("newhost_list.xlsx")
-
-person_mail = set()
-for i in mail_set:
-    li = i.split(",")
-    for j in li:
-        person_mail.add("{}@sogou-inc.com".format(j.strip()))
-print(";".join(list(person_mail)))
-print("done!")
+def read_excel_row(excelname):
+    li = []
+    hash_set = set()
+    workbook = xlrd.open_workbook(excelname)
+    sheet = workbook.sheet_by_name('Sheet1')
+    for i in range(1, sheet.nrows):
+        row = sheet.row_values(i)
+        topic = row[2]
+        if topic in hash_set:
+            continue
+        hash_set.add(topic)
+        li.append(row)
+    return li
